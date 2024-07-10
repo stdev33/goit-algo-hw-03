e5e5d2ebebfe919521837a8c5424ff866aeb19a1
Serhii Tarasenko <stdev33@gmail.com>
2024-07-10 17:44:53 +0300

    task 1. implement recursively copy and sort files by extensions

--- task_1.py
+++ task_1.py
@@ -1,40 +1,55 @@
-import queue
-import random
-import time
-
-request_queue = queue.Queue()
-request_id_counter = 1
+import os
+import shutil
+import argparse
 
 
-def generate_request():
-    global request_id_counter
+def copy_and_sort_files(src_dir, dest_dir):
+    try:
+        for item in os.listdir(src_dir):
+            src_path = os.path.join(src_dir, item)
 
-    request = f"Request {request_id_counter}"
-    request_id_counter += 1
+            if os.path.isdir(src_path):
+                new_dest_dir = os.path.join(dest_dir, item)
 
-    request_queue.put(request)
-    print(f"Generated and added to queue: {request}")
+                if not os.path.exists(new_dest_dir):
+                    os.makedirs(new_dest_dir)
 
+                copy_and_sort_files(src_path, new_dest_dir)
+            elif os.path.isfile(src_path):
+                file_ext = os.path.splitext(item)[1][1:]
+                ext_dir = os.path.join(dest_dir, file_ext)
 
-def process_request():
-    if not request_queue.empty():
-        request = request_queue.get()
+                if not os.path.exists(ext_dir):
+                    os.makedirs(ext_dir)
 
-        print(f"Processing {request}")
-        time.sleep(1)
-    else:
-        print("No requests available to process.")
+                shutil.copy2(src_path, ext_dir)
+                print(f"Copied {src_path} to {ext_dir}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def main():
-    try:
-        while True:
-            generate_request()
-            time.sleep(random.uniform(0.5, 2))
+    parser = argparse.ArgumentParser(description="Recursively copy and sort files by extension.")
+    parser.add_argument("src_dir", help="Source directory")
+    parser.add_argument("dest_dir", nargs='?', default="dist", help="Destination directory (default: dist)")
+    args = parser.parse_args()
 
-            process_request()
-    except KeyboardInterrupt:
-        print("\nExiting the program...")
+    src_dir = args.src_dir
+    dest_dir = args.dest_dir
+
+    if dest_dir == "dist":
+        dest_dir = os.path.join(os.getcwd(), dest_dir)
+
+    if not os.path.exists(src_dir):
+        print(f"Source directory '{src_dir}' does not exist.")
+        return
+
+    if not os.path.exists(dest_dir):
+        print(f"Destination directory '{dest_dir}' does not exist. Creating directory...")
+        os.makedirs(dest_dir)
+
+    copy_and_sort_files(src_dir, dest_dir)
+    print("File copy and sorting completed.")
 
 
 if __name__ == "__main__":
